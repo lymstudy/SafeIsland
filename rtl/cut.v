dc69de97cb1643befea340b7e389b41a108382fa
ymliu <126379387+lymstudy@users.noreply.github.com>
2026-04-17 18:42:11 +0800

    simple cut

--- rtl/cut.v
+++ rtl/cut.v
@@ -1,3 +1,19 @@
-module x()
+module simple_cut (
+    input  wire       clk,
+    input  wire       rst_n,
+    input  wire       en,
+    input  wire [3:0] data_in,
+    output reg  [3:0] data_out,
+    output wire       alarm
+);
+
+always @(posedge clk or negedge rst_n) begin
+    if (!rst_n)
+        data_out <= 4'b0000;
+    else if (en)
+        data_out <= data_in + 4'b0001;
+end
+
+assign alarm = (data_out == 4'b1111);
 
 endmodule
